b4973196a6d783be6304a1b6129294f63dab204c
Edin Kapicm <edin.kapic@vista.com>
2024-05-15 07:04:22 +0200

    functional components

--- src/webparts/recentlyVisitedSites/components/IRecentlyVisitedSitesState.ts
+++
@@ -1,8 +0,0 @@
-import { IWebs } from ".";
-
-export interface IRecentlyVisitedSitesState {
-  usedSites: IWebs[];
-  error: string;
-  loading: boolean;
-}
-
--- src/webparts/recentlyVisitedSites/components/RecentlyVisitedSites.tsx
+++ src/webparts/recentlyVisitedSites/components/RecentlyVisitedSites.tsx
@@ -1,102 +1,86 @@
 import * as React from 'react';
-import styles from './RecentlyVisitedSites.module.scss';
+import { useState, useEffect } from "react";
+import styles from "./RecentlyVisitedSites.module.scss";
 import { WebPartTitle } from "@pnp/spfx-controls-react/lib/WebPartTitle";
 import * as strings from "RecentlyVisitedSitesWebPartStrings";
 import { Link } from "@fluentui/react/lib/Link";
-import { IRecentlyVisitedSitesProps, IRecentlyVisitedSitesState } from ".";
+import { IRecentlyVisitedSitesProps } from ".";
 import { Spinner, SpinnerSize } from "@fluentui/react/lib/Spinner";
 
-export class RecentlyVisitedSites extends React.Component<
-  IRecentlyVisitedSitesProps,
-  IRecentlyVisitedSitesState
-> {
-  /**
-   * Constructor
-   * @param props
-   */
-  constructor(props: IRecentlyVisitedSitesProps) {
-    super(props);
-
-    this.state = {
-      usedSites: [],
-      error: null,
-      loading: true,
-    };
-  }
+export const RecentlyVisitedSites: React.FC<IRecentlyVisitedSitesProps> = (
+  props: IRecentlyVisitedSitesProps
+) => {
+  const [loading, setLoading] = useState(true);
+  const [usedSites, setUsedSites] = useState([]);
+  const [error, setError] = useState(null);
 
   /**
    * componentDidMount lifecycle hook
    */
-  public componentDidMount(): void {
-    this.props.graphService
+  useEffect(() => {
+    props.graphService
       .getRecentVisitedSites()
-      .then((sites) => this.setState({ usedSites: sites, loading: false }))
-      .catch((err) =>
-        this.setState({ error: err, loading: false, usedSites: [] })
-      );
-  }
+      .then((sites) => {
+        setUsedSites(sites);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+        setUsedSites([]);
+      });
+  }, []);
 
-  /**
-   * Default React render method
-   */
-  public render(): React.ReactElement<IRecentlyVisitedSitesProps> {
-    //
-    // supporting different themes for page's section
-    //
-    const color: string | null =
-      (!!this.props.themeVariant &&
-        this.props.themeVariant.semanticColors.bodyText) ||
-      null;
-    const backgroundColor: string | null =
-      (!!this.props.themeVariant &&
-        this.props.themeVariant.semanticColors.bodyBackground) ||
-      null;
+  const color: string | null =
+    (!!props.themeVariant && props.themeVariant.semanticColors.bodyText) ||
+    null;
+  const backgroundColor: string | null =
+    (!!props.themeVariant &&
+      props.themeVariant.semanticColors.bodyBackground) ||
+    null;
 
-    return (
-      <div className={styles.recentlyVisitedSites}>
-        <WebPartTitle
-          displayMode={this.props.displayMode}
-          title={this.props.title}
-          updateProperty={this.props.updateProperty}
-          themeVariant={this.props.themeVariant}
-        />
+  return (
+    <div className={styles.recentlyVisitedSites}>
+      <WebPartTitle
+        displayMode={props.displayMode}
+        title={props.title}
+        updateProperty={props.updateProperty}
+        themeVariant={props.themeVariant}
+      />
 
-        {this.state.loading && (
-          <Spinner label={strings.Loading} size={SpinnerSize.large} />
-        )}
+      {loading && <Spinner label={strings.Loading} size={SpinnerSize.large} />}
 
-        {this.state.usedSites && this.state.usedSites.length > 0 ? (
-          <div
-            className={styles.list}
-            style={{ backgroundColor: backgroundColor }}
-          >
-            <ul>
-              {this.state.usedSites.map((site) => (
-                <li
-                  key={site.id}
-                  className={styles.site}
-                  style={{ background: backgroundColor }}
+      {usedSites && usedSites.length > 0 ? (
+        <div
+          className={styles.list}
+          style={{ backgroundColor: backgroundColor }}
+        >
+          <ul>
+            {usedSites.map((site) => (
+              <li
+                key={site.id}
+                className={styles.site}
+                style={{ background: backgroundColor }}
+              >
+                <Link
+                  href={site.path}
+                  title={site.title}
+                  style={{ color: color }}
                 >
-                  <Link
-                    href={site.path}
-                    title={site.title}
-                    style={{ color: color }}
-                  >
-                    {site.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  {site.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        !loading &&
+        (error ? (
+          <span className={styles.error}>{error}</span>
         ) : (
-          !this.state.loading &&
-          (this.state.error ? (
-            <span className={styles.error}>{this.state.error}</span>
-          ) : (
-            <span className={styles.noSites}>{strings.NoRecentSitesMsg}</span>
-          ))
-        )}
-      </div>
-    );
-  }
-}
+          <span className={styles.noSites}>{strings.NoRecentSitesMsg}</span>
+        ))
+      )}
+    </div>
+  );
+};
