1ec3594baedf1ae9fe16560da5a94059c2e66f5c
Edin Kapicm <edin.kapic@vista.com>
2024-10-13 15:21:07 +0200

    wip (fails on RESX)

---
+++ vitest.config.ts
@@ -0,0 +1,83 @@
+import { defineConfig, configDefaults } from "vitest/config";
+import babel from "vite-plugin-babel";
+import commonjs from "vite-plugin-commonjs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const locStringPath = path.resolve(
+  __dirname,
+  "./src/webparts/recentlyVisitedSites/loc/en-us.js"
+);
+const locPnpStringPath = path.resolve(
+  __dirname,
+  "./node_modules/@pnp/spfx-controls-react/lib/loc/en-us.js"
+);
+
+export default defineConfig({
+  plugins: [
+    babel({
+      filter: /\.(js|jsx)$/, // /\.jsx?$/, // /^.+\\.js$/,
+      include: [locPnpStringPath, locStringPath, "@pnp/spfx-controls-react"],
+      babelConfig: {
+        plugins: ["transform-amd-to-commonjs"],
+      },
+    }),
+    commonjs({
+      filter: (id) =>
+        id.includes("en-us.js") || id.includes("@pnp/spfx-controls-react"),
+    }),
+  ],
+  test: {
+    setupFiles: "./vitest-setup.ts",
+    exclude: [...configDefaults.exclude, "lib", "dist"],
+    environment: "jsdom",
+    server: {
+      deps: {
+        inline: ["@pnp/spfx-controls-react", "en-us.js"],
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@ms/odsp-core-bundle",
+        replacement: "identity-obj-proxy",
+      },
+      {
+        find: /.*\.(?:scss|sass|css|resx)$/,
+        replacement: "identity-obj-proxy",
+        /*replacement: fileURLToPath(
+          new URL(
+            "./node_modules/identity-obj-proxy/src/index.js",
+            import.meta.url
+          )
+        )*/
+      },
+      // {
+      //   find: "\\.(png|jpg)$",
+      //   replacement: path.resolve(
+      //     __dirname,
+      //     "./src/webparts/recentlyVisitedSites/tests/file-stub.ts"
+      //   ),
+      // },
+      {
+        find: "RecentlyVisitedSitesWebPartStrings",
+        replacement: path.resolve(
+          __dirname,
+          "./src/webparts/recentlyVisitedSites/loc/en-us.js"
+        ),
+      },
+      {
+        find: "ControlStrings",
+        replacement: path.resolve(
+          __dirname,
+          "./node_modules/@pnp/spfx-controls-react/lib/loc/en-us.js"
+        ),
+      },
+      //   {
+      //     find: "@/",
+      //     replacement: fileURLToPath(new URL("./", import.meta.url)),
+      //   },
+    ],
+  },
+});
